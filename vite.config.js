75ace727ba22411d2e32707de0e5b5ce247191d0
YovanJCD <yovanjcd@icloud.com>
2025-06-23 13:02:09 +0000

    Actualizar configuración de Vite para ajustar la base del proyecto

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Cambia esto a '/NOMBRE-DEL-REPO/' si tu repo no está en la raíz de tu usuario
 })
