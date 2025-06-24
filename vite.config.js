be93140dc306af4ed6d1231628ac3308e3b583ac
YovanJCD <yovanjcd@icloud.com>
2025-06-24 18:41:28 +0000

    PAGINA

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './PORTFOLIO', // Cambia esto a '/NOMBRE-DEL-REPO/' si tu repo no está en la raíz de tu usuario
-})
